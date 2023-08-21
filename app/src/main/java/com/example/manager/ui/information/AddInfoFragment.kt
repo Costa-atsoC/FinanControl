c804c62b7d1a3a802855bee29d13bc487e67c569
Costa-atsoC <gonca.costa30@gmail.com>
2023-08-21 19:34:01 +0100

    Add Information and start logic

--- app/src/main/java/com/example/manager/ui/information/AddInfoFragment.kt
+++ app/src/main/java/com/example/manager/ui/information/AddInfoFragment.kt
@@ -1,25 +1,55 @@
 package com.example.manager.ui.information
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.navigation.findNavController
 import com.example.manager.R
+import com.example.manager.databinding.FragmentAddInfoBinding
+import com.example.manager.databinding.FragmentInformationBinding
 
 class AddInfoFragment : Fragment() {
+    private var _binding: FragmentAddInfoBinding? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_info, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentAddInfoBinding.inflate(inflater, container, false)
+        return _binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var statusWin: Boolean
+
+        _binding!!.addBack.setOnClickListener{
+            view.findNavController().navigate(R.id.action_addInfoFragment_to_informationFragment)
+        }
+
+        _binding!!.addWin.setOnCheckedChangeListener { _, isChecked ->
+            statusWin = isChecked
+            switchMode(statusWin)
+        }
+    }
+
+    private fun switchMode(statusWin: Boolean){
+        _binding!!.addGroceries.isEnabled = statusWin
+        _binding!!.addDrink.isEnabled = statusWin
+        _binding!!.addOther.isEnabled = statusWin
+
+        var radioButton : RadioButton
+        _binding!!.addCategories.setOnCheckedChangeListener { _, checkedId ->
+            radioButton = requireView().findViewById(checkedId)
+            radioButton.isSelected = statusWin
+        }
     }
 
 }
--- app/src/main/java/com/example/manager/ui/information/InformationFragment.kt
+++ app/src/main/java/com/example/manager/ui/information/InformationFragment.kt
@@ -1,6 +1,7 @@
 package com.example.manager.ui.information
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -30,7 +31,7 @@ class InformationFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d("Begin", "Information")
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -44,7 +45,11 @@ class InformationFragment : Fragment() {
         getInformation()
 
         _binding!!.infoAdd.setOnClickListener {
-            view.findNavController().navigate(R.id.action_informationFragment_to_addInfoFragment)
+            try {
+                view.findNavController().navigate(R.id.action_informationFragment_to_addInfoFragment)
+            } catch (e: Exception) {
+                Log.e("Navigation", "Navigation error: ${e.message}")
+            }
         }
     }
 
@@ -53,6 +58,7 @@ class InformationFragment : Fragment() {
         val recycler = _binding!!.infoRecycler
         val warn = _binding!!.infoWarn
         val add = _binding!!.infoAdd
+
         if(load.visibility == View.VISIBLE){
             recycler.visibility = View.VISIBLE
             warn.visibility = View.VISIBLE
@@ -78,21 +84,17 @@ class InformationFragment : Fragment() {
                     val adapter = CustomAdapterInformation(data)
                     recycler.adapter = adapter
                 }
-                loading()
-
             }
 
             override fun onInformationError() {
                 warn.text = getString(R.string.errorInfo)
-                loading()
-
             }
 
             override fun onInformationNotFound() {
                 warn.text = getString(R.string.noInfo)
-                loading()
-
             }
         })
+        loading()
+
     }
 }
